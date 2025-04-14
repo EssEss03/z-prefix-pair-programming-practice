f063e7d89bf4c5ed126cfe9154fc0f3f6829e8a6
EssEss03 <166960786+EssEss03@users.noreply.github.com>
2025-04-14 12:27:45 -0400

    Update blog.js
    Finished the second test

--- tdd-blog/blog.js
+++ tdd-blog/blog.js
@@ -7,7 +7,13 @@ class Blog {
     if (this.posts.length === 0) {
       return "There are 0 blog posts";
     }
+    return this.posts.map(post => post.title);
+  }
+
+  addBlogPost(title, author = null, genre = null) {
+    cont post = {title, author, genre, comments: [] };
+    this.posts.push(post);
   }
 }
 
-module.exports = Blog
+module.exports = Blog;
