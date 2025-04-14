3efe68ea6813ecb58533b64261c950f862c0ddf2
EssEss03 <166960786+EssEss03@users.noreply.github.com>
2025-04-14 12:57:12 -0400

    Update blog.test.js

--- tdd-blog/spec/blog.test.js
+++ tdd-blog/spec/blog.test.js
@@ -72,30 +72,30 @@ describe('Blog', () => {
   })
 
 
-  // it('it allows a user to add comments to a blog', () => {
-  //   let blog = new Blog();
+  it('it allows a user to add comments to a blog', () => {
+    let blog = new Blog();
 
-  //   blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
-  //   blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
-  //   expect(blog.getBlogComments("How to Swim Fast")).toEqual([])
+    blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
+    blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
+    expect(blog.getBlogComments("How to Swim Fast")).toEqual([])
 
-  //   blog.addComment("How to Swim Fast", "katiehoesley", "I hate this article!")
-  //   blog.addComment("I Love Football!", "ashcoca", "I love football too! Lol!")
-  //   blog.addComment("I Love Football!", "brandon_ck", "GO FALCONS!")
-  //   expect(blog.getBlogComments("How to Swim Fast")).toEqual([
-  //     {
-  //       comments: [{ user: "katiehoesley", comment: "I hate this article!" }]
-  //     }
-  //   ])
-  //   expect(blog.getBlogComments("I Love Football!")).toEqual([
-  //     {
-  //       comments: [
-  //         { user: "ashcoca",comment: "I love football too! Lol!" }, 
-  //         { user: "brandon_ck", comment: "GO FALCONS!" }
-  //       ]
-  //     }, 
-  //   ])
-  // })
+    blog.addComment("How to Swim Fast", "katiehoesley", "I hate this article!")
+    blog.addComment("I Love Football!", "ashcoca", "I love football too! Lol!")
+    blog.addComment("I Love Football!", "brandon_ck", "GO FALCONS!")
+    expect(blog.getBlogComments("How to Swim Fast")).toEqual([
+      {
+        comments: [{ user: "katiehoesley", comment: "I hate this article!" }]
+      }
+    ])
+    expect(blog.getBlogComments("I Love Football!")).toEqual([
+      {
+        comments: [
+          { user: "ashcoca",comment: "I love football too! Lol!" }, 
+          { user: "brandon_ck", comment: "GO FALCONS!" }
+        ]
+      }, 
+    ])
+  })
 
 
   // it('allows you to see all comments written by a specific user', () => {
