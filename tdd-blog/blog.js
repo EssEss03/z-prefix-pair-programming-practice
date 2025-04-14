fcd9250936730a1e11b4cc6d3632dec979e4e286
EssEss03 <166960786+EssEss03@users.noreply.github.com>
2025-04-14 12:21:21 -0400

    Update blog.js
    Finished first test

--- tdd-blog/blog.js
+++ tdd-blog/blog.js
@@ -1,9 +1,13 @@
 class Blog {
   constructor() {
-
+    this.posts = []; // store blog posts here
   }
 
-
+  getBlogPosts() {
+    if (this.posts.length === 0) {
+      return "There are 0 blog posts";
+    }
+  }
 }
 
 module.exports = Blog
